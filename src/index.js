b22bdb4ab472aa880165ee7d57ff423fa3425e23
Mason Embry <mason@embrycode.com>
2016-12-15 15:56:18 -0600

    Routes are written sending verification text

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 // load modules
 var express = require('express');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var app = express();
+var routes = require('./routes');
 
 // set our port
 app.set('port', process.env.PORT || 5000);
@@ -12,10 +14,16 @@ app.set('port', process.env.PORT || 5000);
 // morgan gives us http request logging
 app.use(morgan('dev'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // setup our static route to serve files from the "public" folder
 app.use('/', express.static('public'));
 
+// use routes.js for all routes, mounted on /api
+app.use('/api', routes);
+
 // start listening on our port
 var server = app.listen(app.get('port'), function() {
-  console.log('Express server is listening on port ' + server.address().port);  
+  console.log('Express server is listening on port ' + server.address().port);
 });
---
+++ src/routes.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+// GET /api/courses 200 - Returns the Course "_id" and "title" properties
+router.get('/courses', function(req, res, next) {
+  res.send('This is the courses route.');
+});
+
+// GET /api/course/:id 200 - Returns all Course properties and related documents for the provided course ID
+router.get('/courses/:id', function(req, res, next) {
+  res.send('This is the route for a specific course by ID.');
+});
+
+// POST /api/courses 201 - Creates a course, sets the Location header, and returns no content
+router.post('/courses', function(req, res, next) {
+  res.send('This is the route for creating a course.');
+});
+
+// PUT /api/courses/:id 204 - Updates a course and returns no content
+router.put('/courses/:id', function(req, res, next) {
+  res.send('This is the route for updating a course.');
+});
+
+// GET /api/users 200 - Returns the currently authenticated user
+router.get('/users', function(req, res, next) {
+  res.send('This is the route for returning the currently authenticated user.');
+});
+
+// POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
+router.post('/users', function(req, res, next) {
+  res.send('This is the route for creating a user.');
+});
+
+// POST /api/courses/:courseId/reviews 201 - Creates a review for the specified course ID, sets the Location header to the related course, and returns no content
+router.post('/courses/:courseId/reviews', function(req, res, next) {
+  res.send('This is the route for creating a review for the specified course.');
+});
+
+// DELETE /api/courses/:courseId/reviews/:id 204 - Deletes the specified review and returns no content
+router.delete('/courses/:courseId/reviews/:id', function(req, res, next) {
+  res.send('This is the route for deleting the specified review.');
+});
+
+module.exports = router;
