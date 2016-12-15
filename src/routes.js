5405dd8c94f72b5af5f8e96ffb461bfd73c2ea0c
Mason Embry <mason@embrycode.com>
2016-12-15 17:07:50 -0600

    Working on returning mock data

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,21 @@ var router = express.Router();
 
 // GET /api/courses 200 - Returns the Course "_id" and "title" properties
 router.get('/courses', function(req, res, next) {
-  res.send('This is the courses route.');
+  res.json([
+    {
+      _id: "1234",
+      title: "Title1"
+    },
+    {
+      _id: "12345",
+      title: "Title2"
+    },
+    {
+      _id: "123456",
+      title: "Title3"
+    }
+  ]);
+  //res.send('This is the courses route.');
 });
 
 // GET /api/course/:id 200 - Returns all Course properties and related documents for the provided course ID
@@ -15,11 +29,14 @@ router.get('/courses/:id', function(req, res, next) {
 
 // POST /api/courses 201 - Creates a course, sets the Location header, and returns no content
 router.post('/courses', function(req, res, next) {
+  res.status(201);
+  res.location('/courses');
   res.send('This is the route for creating a course.');
 });
 
 // PUT /api/courses/:id 204 - Updates a course and returns no content
 router.put('/courses/:id', function(req, res, next) {
+  res.status(204);
   res.send('This is the route for updating a course.');
 });
 
@@ -30,16 +47,21 @@ router.get('/users', function(req, res, next) {
 
 // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
 router.post('/users', function(req, res, next) {
+  res.status(201);
+  res.location('/');
   res.send('This is the route for creating a user.');
 });
 
 // POST /api/courses/:courseId/reviews 201 - Creates a review for the specified course ID, sets the Location header to the related course, and returns no content
 router.post('/courses/:courseId/reviews', function(req, res, next) {
+  res.status(201);
+  res.location('/courses/' + req.params.courseId);
   res.send('This is the route for creating a review for the specified course.');
 });
 
 // DELETE /api/courses/:courseId/reviews/:id 204 - Deletes the specified review and returns no content
 router.delete('/courses/:courseId/reviews/:id', function(req, res, next) {
+  res.status(204);
   res.send('This is the route for deleting the specified review.');
 });
 
